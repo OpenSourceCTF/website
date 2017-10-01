afbaa1722b9a9dbe581f14790ec9f270605fa175
bicycle <steven.allaben@gmail.com>
2017-10-01 18:41:08 -0400

    fullscreen toggle & /arena/ formatting
    Added a basic user interface to /arena/. The fullscreen function still
    needs work to scale to the proper size (although this might be my
    doing). needs code police! also, updated ctfpro to osctf in several
    places.

--- client/pages/arena/arena.js
+++ client/pages/arena/arena.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
-import InitGame from 'ctfpro-web-client'
-
+import PrimaryNav from 'Components/nav/primary/'
+import { renderer, makeFullscreen } from 'ctfpro-web-client'
 import styles from './arena.sass'
 
 class Arena extends Component {
 	componentDidMount () {
-		InitGame(this.gameEl)
+		this.gameEl.appendChild(renderer.view)
 	}
 
 	render () {
 		return (
-			<main
-				className={styles['wrapper']}
-				ref={el => { this.gameEl = el }}
-			/>
+			<div>
+				<PrimaryNav />
+				<main
+					className={styles['wrapper']}
+					ref={el => { this.gameEl = el }}
+				/>
+				<div id={styles['fullscreen']}>
+					<button onClick={makeFullscreen}>
+						Fullscreen
+					</button>
+				</div>
+			</div>
 		)
 	}
 }
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = new Map()
 
-config.set('APP_NAME', 'CTF Pro')
+config.set('APP_NAME', 'OS CTF')
 
 // CJS so that it can be isomorphic
 module.exports = config
